4c812397b7e755a730822488a45571ec3f934327
Kellojo <12833426+Kellojo@users.noreply.github.com>
2025-09-21 11:15:04 +0200

    Added auto reload, on failed markdown parse

--- src/server.js
+++ src/server.js
@@ -13,8 +13,26 @@ reloadServer.watch("docs");
 app.use(require("connect-livereload")());
 
 app.get("/", (req, res) => {
-    const html = builder.buildDocs({ saveToDisk: false });
+    try {
+        const html = builder.buildDocs({ saveToDisk: false });
     res.send(html);
+    } catch (error) {
+        const html = `
+            <h1>Error building docs</h1>
+            <span>This is likely due to a syntax error in your markdown files.</span>
+            <pre>${error.message}</pre>
+            <pre>${error.stack}</pre>
+
+            <script>
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
+            </script>
+        `;
+        res.send(html);
+    }
+
+    
 });
 
 app.listen(PORT, () => {
